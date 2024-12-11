fc372ec164272ce24a74a2629e3634abbe6f1083
hundredvisionsguy <winikkc@hsd.k12.or.us>
2024-12-11 09:18:59 -0800

    adding initial project files

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println();
+        
+        // Greet user
+
+        // Instantiate your objects
+        Rectangle rect1 = new Rectangle(100, 200);
+
+        // Print your objects
+        System.out.println(rect1 + "\n");
+    }
+}
